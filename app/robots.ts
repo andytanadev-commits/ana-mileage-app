09028969addcc8cebc7b831c5f467c79c251bf1c
AT Corp - Takahiro Anzai <atcorp-andytaka@mac.lan>
2026-09-08 09:54:09 +0900

    SEO対策: sitemap.tsおよびrobots.tsを追加

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://ana-mileage-app.vercel.app//sitemap.xml', // ★本番のURLに変更
+  };
+}
